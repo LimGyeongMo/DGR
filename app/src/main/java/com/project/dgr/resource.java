86ee1677c74fcb6db26df2fd4d6231b1be0bf904
LimGyeongMo <133728406+LimGyeongMo@users.noreply.github.com>
2024-07-22 14:10:00 +0900

    1

---
+++ app/src/main/java/com/project/dgr/resource.java
@@ -0,0 +1,25 @@
+package com.project.dgr;
+
+import android.content.Context;
+
+import androidx.core.content.ContextCompat;
+
+public class resource {
+
+    public final static String PRIMARY_700 = "PRIMARY_700";
+    public final static String PRIMARY_500 = "PRIMARY_500";
+    public final static String PRIMARY_200 = "PRIMARY_200";
+    public final static String PRIMARY_100 = "PRIMARY_100";
+    public final static String PRIMARY_50 = "PRIMARY_50";
+
+    public int getColor(Context context, String colorName){
+        switch (colorName) {
+            case PRIMARY_700: return ContextCompat.getColor(context, R.color.color_00BCFF);
+            case PRIMARY_500: return ContextCompat.getColor(context, R.color.color_01CAFF);
+            case PRIMARY_200: return ContextCompat.getColor(context, R.color.color_70D8FF);
+            case PRIMARY_100: return  ContextCompat.getColor(context, R.color.color_ACE7FE);
+            case PRIMARY_50: return ContextCompat.getColor(context, R.color.color_DFF6FF);
+        }
+        return 0;
+    }
+}
